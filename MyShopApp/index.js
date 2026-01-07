25861fa6ed675dd96d66547a2f310d2ccd062727
MyShop Developer <developer@myshop.app>
2026-01-07 11:33:32 +0530

    ok

--- MyShopApp/index.js
+++
@@ -1,7 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './index.tsx';
-
-console.log('🔴 index.js: AppRegistry registering app');
-
-AppRegistry.registerComponent('main', () => App);
-
